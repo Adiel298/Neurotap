60c95ad7fd605a5734c622fcbfb6edf8efb2cfc0
Adiel298 <engadielmitch@gmail.com>
2025-10-30 19:59:02 +0300

    firebas.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4krwLdtZ1axVU9ioM7WutZqtUbJEN6Gg",
+  authDomain: "neurotap-c9649.firebaseapp.com",
+  projectId: "neurotap-c9649",
+  storageBucket: "neurotap-c9649.firebasestorage.app",
+  messagingSenderId: "505876683169",
+  appId: "1:505876683169:web:3b3923533f5cbe7f6a93e0",
+  measurementId: "G-K9ZDJT2HWN"
+};
+// 
+
+// For now, this file exists to be filled in when you connect Auth + Firestore.
