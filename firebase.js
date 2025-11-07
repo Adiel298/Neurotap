4f49922da23a7316c9bac91fb9e32ba796a3650d
Adiel298 <engadielmitch@gmail.com>
2025-11-07 15:16:18 +0000

    Fix Firebase config (correct storage bucket)

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   authDomain: "neurotap-c9649.firebaseapp.com",
   databaseURL: "https://neurotap-c9649-default-rtdb.europe-west1.firebasedatabase.app", // ✅ correct Realtime DB URL
   projectId: "neurotap-c9649",
-  storageBucket: "neurotap-c9649.firebasestorage.app", // ✅ correct storage bucket
+  storageBucket: "neurotap-c9649.appspot.com", // ✅ fixed storage bucket
   messagingSenderId: "505876683169",
   appId: "1:505876683169:web:3b3923533f5cbe7f6a93e0",
   measurementId: "G-K9ZDJT2HWN"
